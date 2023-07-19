b050a3637c92bd2914e0abb77f94a6f36ee4f862
Meena <meenakk2007@gmail.com>
2023-07-19 09:47:54 -0400

    x

---
+++ Mastermind.js
@@ -0,0 +1,25 @@
+let reset = document.getElementById("reset");
+reset.addEventListener('click', resetGame, false);
+let secretCode = [];
+let colors = ["red", "blue", "yellow", "green", "orange", "purple"];
+/****  WRITE A COMMAND HERE THAT WILL RESET THE GAME USING resetGame() *****/
+
+function resetGame() {
+    /****  WRITE A COMMAND HERE THAT WILL PICK A RANDOM CODE THE GAME *****/
+    /****  USING THE FUNCTION YOU DEFINE BELOW *****/
+    secretCode =[];
+    for(let i = 0; i<4;i++){
+        let randomIndex= Math.floor(Math.random()*(colors.length));
+        secretCode.push(colors[randomIndex]);
+    }
+
+    console.log(secretCode);
+}
+
+/*** CREATE A FUNCTION HERE THAT WILL PICK A RANDOM 4-COLOR CODE AND ****/
+/***** STORE IT AS AN ARRAY IN THE VARIABLE NAMED secretCode. ******/
+//For example, a valid value for secretCode could be ["blue", "red", "green", "blue"]
+
+    //first make sure there is nothing in the secretCode array by making it an empty array
+
+    //push 4 random strings from the colors array into the secretCode array
